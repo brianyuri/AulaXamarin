a6aaf87df00570e6c617a9cf1deba41e3124b572
Brian Yuri Fachini <brianyurif17@gmail.com>
2020-02-09 18:41:15 -0300

    Aula de Xamarin

---
+++ Aula1/Aula1/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Aula1
+{
+    
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            Image.Source
+                //http://dontpad.com/xamarinformsproway
+
+        }
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            List<string> nomes = new List<string>();
+            nomes.Add(Nome.Text);
+            List.Item
+        }
+
+        public void Entry_Focused(object sender, FocusEventArgs e)
+        {
+            
+
+        }
+    }
+}
